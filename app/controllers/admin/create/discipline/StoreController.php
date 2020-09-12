f4377abeac75618a37bd0607395d678a0f63e36c
Lailson Conceição <lailsonconceicaodv@gmail.com>
2020-09-12 11:24:36 -0300

    <feat>: adição de slug para listagem na view de cadastro de professores

--- app/controllers/admin/create/discipline/StoreController.php
+++ app/controllers/admin/create/discipline/StoreController.php
@@ -4,16 +4,21 @@ use App\Models\admin\discipline\Discipline;
 
 $filtedInput = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 
+$clearedString = clearString($filtedInput);
+
 if (!empty($filtedInput)) {
     $discipline = new Discipline;
-    $name = ['name' => $filtedInput];
+    $data = [
+        'name' => $filtedInput,
+        'slug' => clearString($filtedInput)
+    ];
 
     $result = $discipline->find('name', $filtedInput);
 
     if (count((array)$result) > 1)
         return header('Location: /cadastrar/disciplina');
 
-    $discipline->save($name);
+    $discipline->save($data);
 }
 
 header('Location: /cadastrar/disciplina');
